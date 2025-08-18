a65b0cec3c145a473c0460f3499d4310883e00de
szchinismo <szchinoelsabio016@gmail.com>
2025-08-18 15:31:32 +0000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~IjVzzbhC#xVEF5RP1vSpjETx8DqSr34pNGDvRgc1B_folSwphg0o",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "2250715405077", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
